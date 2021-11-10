a89ddcc358f36dca9a3759e016b0f9be9379743c
Romain Clement <contact@romain-clement.net>
2021-11-10 08:41:39 +0100

    Update speech synthesis lab

--- lab/04-speech-synthesis/speech_synthesis_solution.py
+++ lab/04-speech-synthesis/speech_synthesis_solution.py
@@ -14,10 +14,16 @@ cog_region = os.getenv("COG_REGION")
 
 
 def speech_synthesis(text: str) -> None:
-    # Instantiate Cognitive Services credentials using `cog_key`
+    # Instantiate Cognitive Services Speech Config `cog_key` and `cog_region`
     speech_config = speech.SpeechConfig(cog_key, cog_region)
+
+    # Select the preferred synthesis voice
     speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+
+    # Instantiate a Speech Synthesizer with the given config
     speech_synthesizer = speech.SpeechSynthesizer(speech_config)
+
+    # Run the text-to-speech with the input text
     speak = speech_synthesizer.speak_text_async(text).get()
     if speak.reason != speech.ResultReason.SynthesizingAudioCompleted:
         print(speak.reason)
